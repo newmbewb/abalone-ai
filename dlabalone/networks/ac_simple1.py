c6d8c145e55163da982ad145bbb84a3e243b1067
Jaehyung Ahn <jh2ekd@gmail.com>
2022-05-31 20:54:30 +0900

    Freeze first model

--- dlabalone/networks/ac_simple1.py
+++ dlabalone/networks/ac_simple1.py
@@ -9,96 +9,126 @@ from keras.models import Sequential
 from keras.layers import Dropout
 from keras.metrics import RootMeanSquaredError
 from keras.losses import Huber
+from tensorflow.keras.optimizers import SGD
+import tensorflow.keras.backend as K
+from tensorflow.keras.utils import get_custom_objects
 
 from dlabalone.networks.base import Network
 
 
 class ACSimple1(Network):
-    def __init__(self, mode, dropout_rate=0.1, data_format="channels_last"):
+    def __init__(self, mode, dropout_rate=None, data_format="channels_last"):
         self.mode = mode
         self.dropout_rate = dropout_rate
         self.data_format = data_format
         if mode == 'policy':
+            if dropout_rate is None:
+                self.dropout_rate = 0.3
             name = f"ACSimple1Policy_dropout{dropout_rate}"
         elif mode == 'value':
+            if dropout_rate is None:
+                self.dropout_rate = 0.5
             name = f"ACSimple1Value_dropout{dropout_rate}"
         else:
             assert False, "Wrong mode"
         super().__init__(name)
 
+        # Define mish activation
+        class Mish(Activation):
+            def __init__(self, activation, **kwargs):
+                super(Mish, self).__init__(activation, **kwargs)
+                self.__name__ = 'Mish'
+
+        def mish(x):
+            return x * K.tanh(K.softplus(x))
+
+        get_custom_objects().update({'mish': Mish(mish)})
+
     def model(self, input_shape, num_classes, optimizer=None):
-        model = self._model(input_shape, num_classes, dropout_rate=self.dropout_rate)
-        if optimizer is not None:
-            if self.mode == 'policy':
-                top3_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
-                top3_acc.__name__ = 'top3_acc'
-                top5_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=5)
-                top5_acc.__name__ = 'top5_acc'
-                model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', top3_acc, top5_acc])
-            elif self.mode == 'value':
-                model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mean_squared_error'])
-            else:
-                return None
+        if self.mode == 'policy':
+            model = self._policy_model(input_shape, num_classes, dropout_rate=self.dropout_rate)
+            top3_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
+            top3_acc.__name__ = 'top3_acc'
+            top5_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=5)
+            top5_acc.__name__ = 'top5_acc'
+            if optimizer is None:
+                optimizer = SGD(learning_rate=0.1)
+            model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', top3_acc, top5_acc])
+        elif self.mode == 'value':
+            model = self._value_model(input_shape, dropout_rate=self.dropout_rate)
+            if optimizer is None:
+                optimizer = 'adam'
+            model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mean_squared_error'])
+        else:
+            return None
         return model
 
-    def _model(self, input_shape, num_classes,
-               dropout_rate=0.1):
-
+    def _policy_model(self, input_shape, num_classes, dropout_rate):
         model = Sequential()
-        for layer in self._layers(input_shape, dropout_rate):
+        for layer in self._policy_layers(input_shape, dropout_rate):
             model.add(layer)
 
-        if self.mode == 'policy':
-            model.add(Flatten())
-            model.add(Dense(num_classes, activation='softmax'))
-            # model.add(Activation('softmax'))
-            return model
+        model.add(Flatten())
+        model.add(Dense(num_classes, activation='softmax'))
+        return model
 
-        elif self.mode == 'value':
-            model.add(ZeroPadding2D((2, 2), data_format=self.data_format))
-            model.add(Conv2D(64, (5, 5), data_format=self.data_format))
-            model.add(Activation('relu'))
-            model.add(Dropout(rate=dropout_rate))
-
-            model.add(Flatten())
-            model.add(Dense(256, activation='relu'))
-            model.add(Dense(1, activation='tanh'))
-            return model
-
-    def _layers(self, input_shape, dropout_rate):
+    def _policy_layers(self, input_shape, dropout_rate):
         return [
             ZeroPadding2D((2, 2), input_shape=input_shape, data_format=self.data_format),
             Conv2D(64, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(64, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(64, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(48, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(48, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(32, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
 
             ZeroPadding2D((2, 2), data_format=self.data_format),
             Conv2D(32, (5, 5), data_format=self.data_format),
-            Activation('relu'),
+            Activation('mish'),
+            Dropout(rate=dropout_rate),
+        ]
+
+    def _value_model(self, input_shape, dropout_rate):
+        model = Sequential()
+        for layer in self._value_layers(input_shape, dropout_rate):
+            model.add(layer)
+
+        model.add(Flatten())
+        model.add(Dense(256, activation='mish'))
+        model.add(Dense(1, activation='tanh'))
+        return model
+
+    def _value_layers(self, input_shape, dropout_rate):
+        return [
+            ZeroPadding2D(padding=3, input_shape=input_shape, data_format=self.data_format),
+            Conv2D(32, (5, 5), data_format=self.data_format),
+            Activation('mish'),
+            Dropout(rate=dropout_rate),
+
+            ZeroPadding2D(padding=2, data_format=self.data_format),
+            Conv2D(32, (5, 5), data_format=self.data_format),
+            Activation('mish'),
             Dropout(rate=dropout_rate),
         ]
--- run/test_train.py
+++ run/test_train.py
@@ -7,12 +7,14 @@ from dlabalone.data.data_processor import DataGenerator, DataGeneratorMock
 from dlabalone.encoders.twoplane import TwoPlaneEncoder
 from dlabalone.encoders.threeplane import ThreePlaneEncoder
 from dlabalone.encoders.base import get_encoder_by_name
+from dlabalone.encoding_networks.network_with_encoder import NetworkWithEncoder, get_output_shape
 from dlabalone.networks import simple1, alpha_abalone, ac_simple1, ac_light1
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense
 import os
 import tensorflow as tf
 
+from dlabalone.networks.base import compile_model
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -20,28 +22,46 @@ if __name__ == '__main__':
     # tf.compat.v1.disable_eager_execution()
     multiprocessing.freeze_support()
     mode = 'policy'
-    optimizer = SGD(learning_rate=0.1)
-    # optimizer = 'adam'
-    if mode == 'policy':
-        dropout_rate = 0.10
-    else:
-        dropout_rate = 0.15
-    encoder = get_encoder_by_name('alpha_abalone', 5, mode)
-    # encoder = get_encoder_by_name('fourplane', 5, mode, data_format="channels_last")
-    generator = DataGenerator(encoder, 1024, '../data/data_with_value/dataset', '../data/encoded_data', 0.2,
-                              max_train_npz=400)
+    # optimizer = SGD(learning_rate=0.1)
 
-    # model_generator = ac_simple1.ACSimple1(mode, dropout_rate=dropout_rate, data_format="channels_last")
-    model_generator = ac_light1.ACLight1(mode, dropout_rate=dropout_rate, data_format="channels_last")
+    if mode == 'policy':
+        # optimizer = 'sgd'
+        optimizer = SGD(learning_rate=0.1)
+        dropout_rate = 0.3
+    elif mode == 'value':
+        optimizer = 'adam'
+        # optimizer = SGD(learning_rate=0.1)
+        dropout_rate = 0.5
+    else:
+        assert False, "mode should be 'policy' or 'value'"
+    # encoder = get_encoder_by_name('alpha_abalone', 5, mode, data_format="channels_last")
+    encoder = get_encoder_by_name('fourplane', 5, mode, data_format="channels_last")
+    # generator = DataGenerator(encoder, 1024, '../data/data_with_value/dataset', '../data/encoded_data', mode, 0.2,
+    #                           max_train_npz=200)
+    generator = DataGenerator(
+        encoder, 1024, '../data/rl_mcts/dataset', '../data/rl_mcts/encoded_data', mode, 0.2)
+
+    # Simple network
+    model_generator = ac_simple1.ACSimple1(mode, dropout_rate=dropout_rate, data_format="channels_last")
+    # model_generator = ac_light1.ACLight1(mode, dropout_rate=dropout_rate, data_format="channels_last")
     model = model_generator.model(encoder.shape(), encoder.num_moves(), optimizer=optimizer)
-    # model = load_model('../data/checkpoints/backup/ACSimple1Policy_dropout0.1_AlphaAbaloneEncoder_epoch_14.h5')
+    network_name = f'{model_generator.name()}_{encoder.name()}'
+
+    # # Stack encoder model
+    # encoder_model = load_model('../data/checkpoints/models/EncoderNetworkConcatSimple2_FourPlaneEncoder_channels_last_AlphaAbaloneEncoder_channels_last_epoch_1.h5')
+    # base_model_generator = ac_simple1.ACSimple1(mode, dropout_rate=dropout_rate, data_format="channels_last")
+    # base_model = base_model_generator.model(get_output_shape(encoder_model), encoder.num_moves(), optimizer=optimizer)
+    # model_generator = NetworkWithEncoder(encoder_model, base_model)
+    # model = model_generator.model()
+    # compile_model(model, mode, optimizer)
+    # network_name = f'{base_model_generator.name()}_with_encoder_{encoder.name()}'
 
     # Make network name
     model.summary()
-    network_name = f'{model_generator.name()}_{encoder.name()}'
+    # network_name = f'{model_generator.name()}_{encoder.name()}'
     print(network_name)
     print(f'optimizer: {optimizer}')
-    epochs = 100000
+    epochs = 1000
     model.fit_generator(
         generator=generator.generate('train'),
         epochs=epochs,
